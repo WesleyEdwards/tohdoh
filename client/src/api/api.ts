3d2afa7b08076c74cfa2b5d8a01c71c8d0007029
Joshua McClung <josh.mcclung66@gmail.com>
2023-03-25 12:08:41 -0600

    Fix unscheduled event scheduler url

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -117,7 +117,7 @@ export class Api {
   }
   createUnscheduledEventScheduler(body: CreateUnscheduledEventSchedulerBody) {
     return this.post(
-      "unscheduledEventScheduler/unscheduledEventScheduler",
+      "eventSchedulers/unscheduledEventScheduler",
       body
     );
   }
